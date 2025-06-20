edf2785cec8cba011c9288181196196211bb5383
Antonio Zanon <antonio.zanon99@gmail.com>
2025-06-20 14:05:04 -0300

    Reassistir Aula 3 - Controlando o Estado

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const [valor, setValor] = useState("");
+
+  const aoDigitado = (e) => {
+    setValor(e.target.value);
+    console.log("Valor digitado:", valor);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder}></input>
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={props.placeholder}
+      ></input>
     </div>
   );
 };
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao/Botao";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
 import "./Formulario.css";
@@ -12,14 +13,29 @@ const Formulario = () => {
     "Mobile",
     "Inovação e Gestão",
   ];
+  const aoSalvar = (e) => {
+    e.preventDefault();
+    // Lógica para salvar os dados do formulário
+    console.log("Formulário salvo!");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu Nome" />
-        <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o seu Nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite o seu cargo"
+        />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao texto="Criar Card" />
       </form>
     </section>
   );
