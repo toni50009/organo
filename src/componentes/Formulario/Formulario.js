2b70fd63b157d304fd8400016fc820ff9d30ff58
Antonio Zanon <antonio.zanon99@gmail.com>
2025-06-19 23:57:15 -0300

    Primeiros passos com o projeto em REACT

---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,28 @@
+import CampoTexto from "../CampoTexto/CampoTexto";
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
+import "./Formulario.css";
+
+const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto label="Nome" placeholder="Digite o seu Nome" />
+        <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
